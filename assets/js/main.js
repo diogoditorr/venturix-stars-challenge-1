648fe24b7050fdaf06298945a284a47fc2d0713c
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-07-26 20:07:40 -0300

    feat: improve accessibility with keyboard keys

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
 const submitButton = document.querySelector('.rating-evaluation button.submit');
 const ratingEvaluationSection = document.querySelector('.rating-evaluation');
 const ratingAcknowledgementSection = document.querySelector('.rating-acknowledgement');
-const ratingOptions = document.querySelectorAll('.rating-evaluation ul.rating-options li');
+const ratingOptions = document.querySelectorAll('.rating-evaluation .rating-options span');
 const ratingSelected = document.querySelector('.rating-acknowledgement span.rating-selected');
 let ratingResult = 0
+
+function onRatingOptionPress(ratingOption) {
+    clearRatingOptions();
+
+    const newRatingResult = ratingOption.getAttribute('data-value');
+    if (newRatingResult == ratingResult) {
+        ratingResult = 0;
+        disableSubmitButton();
+        return
+    }
+
+    ratingResult = newRatingResult;
+    enableSubmitButton();
+    ratingOption.classList.add('selected');
+}
 
 function onSubmitButton() {
     ratingSelected.textContent = `You selected ${ratingResult} out of 5`;
@@ -12,8 +27,8 @@ function onSubmitButton() {
 }
 
 function clearRatingOptions() {
-    ratingOptions.forEach((value) => {
-        value.classList.remove('selected');
+    ratingOptions.forEach((ratingOption) => {
+        ratingOption.classList.remove('selected');
     });
 }
 
@@ -26,20 +41,28 @@ function disableSubmitButton() {
 }
 
 ratingOptions.forEach((ratingOption) => {
-    ratingOption.addEventListener('click', () => {
-        clearRatingOptions();
-
-        const newRatingResult = ratingOption.getAttribute('data-value');
-        if (newRatingResult == ratingResult) {
-            ratingResult = 0;
-            disableSubmitButton();
-            return
+    ratingOption.addEventListener('click', () => onRatingOptionPress(ratingOption));
+    ratingOption.onkeydown = (e) => {
+        if (e.keyCode == 13) { // enter
+            onRatingOptionPress(ratingOption);
         }
-
-        ratingResult = newRatingResult;
-        enableSubmitButton();
-        ratingOption.classList.add('selected');
-    });
+        if (e.keyCode == 39) { // arrow-right
+            const nextElement = ratingOption.nextElementSibling;
+            if (nextElement) {
+                ratingOption.tabIndex = -1;
+                nextElement.tabIndex = 0;
+                nextElement.focus();
+            }
+        }
+        if (e.keyCode == 37) { // arrow-left
+            const previousElement = ratingOption.previousElementSibling;
+            if (previousElement) {
+                ratingOption.tabIndex = -1;
+                previousElement.tabIndex = 0;
+                previousElement.focus();
+            }
+        }
+    }
 });
 
 submitButton.addEventListener('click', onSubmitButton);
